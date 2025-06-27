d30326a6b374ee1d90a0697603a196a325ead48e
pressje04 <pressje04@vt.edu>
2025-06-27 13:43:58 -0400

    global layout changes for mobile bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,14 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-black">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white min-h-screen`}
       >
         {children}
-        <Script src="https://sdk.scdn.co/spotify-player.js" strategy="afterInteractive"/>
+        <Script src="https://sdk.scdn.co/spotify-player.js" strategy="afterInteractive" />
         <div className="mt-10">
-        <MusicPlayerBar />
+          <MusicPlayerBar />
         </div>
       </body>
     </html>
