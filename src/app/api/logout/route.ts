c41afca23b75785e2d756820067078f6a5c842be
pressje04 <pressje04@vt.edu>
2025-06-27 19:58:53 -0400

    logout issue

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,19 +1,14 @@
 import {NextResponse, NextRequest} from 'next/server';
 
 export async function POST(req: NextRequest) {
-    const origin = req.headers.get('origin') || 'http://localhost:3000';
-    const response = NextResponse.redirect(`${origin}`);
-
-    response.cookies.set('token', '', {
-        httpOnly: true,
-        maxAge: 0,
-        path: '/'
-    });
-
-    return response;
-}
-
-export async function GET(req: NextRequest) {
-    const origin = req.headers.get('origin') || '/'
-    return NextResponse.redirect(origin)
+    const origin = req.headers.get('origin') || '/';
+    const res = NextResponse.redirect(origin, { status: 303 }); // Changes to GET
+    res.cookies.set('token', '', { httpOnly: true, maxAge: 0, path: '/' });
+    return res;
   }
+  
+  export async function GET(req: NextRequest) {
+    const origin = req.headers.get('origin') || '/';
+    return NextResponse.redirect(origin, { status: 303 });
+  }
+  
